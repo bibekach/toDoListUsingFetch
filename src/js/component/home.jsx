06bbc663ae976969ac41273dc36355cd1ae48aa7
Bibek Acharya <acharya.bbk@gmail.com>
2023-04-30 23:31:37 +0000

    completed add todo and delete todo using fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,71 @@
-import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useEffect, useState } from "react";
+
 
 //create your first component
 const Home = () => {
+	const [newTask, setNewTask] = useState("");
+	const [list, setList] = useState([]);
+
+	useEffect(()=>{
+		getTodos();
+	},[])
+	
+	const getTodos= async ()=>{
+		const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/ramhari");
+		const data = await response.json();
+		setList(data); 
+		console.log(data)
+	}
+
+	const updateTodoList= async ()=>{
+		const templist =[...list, {label:newTask, done:false }];
+		setList(templist);
+		const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/ramhari",{
+			method: "PUT",
+			headers: {"Content-Type": "application/json"},
+			body:JSON.stringify(templist)
+		});
+		if(response.ok){
+			setNewTask("");
+		}
+	}
+
+	const deleteTodo = async (todoToDelete)=>{
+		const templist =list.filter((item, y) => item != todoToDelete)
+		setList(templist);
+		const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/ramhari",{
+		  method: "PUT",
+		  headers: {"Content-Type": "application/json"},
+		  body:JSON.stringify(templist)  
+		});
+	  }
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="row justify-content-center">
+				<div className="col-12 list-group-item">
+					<input className="form-control" value={newTask} placeholder="Add new task" onChange={(e) => { setNewTask(e.target.value) }} onKeyUp={(e) => {
+						if (e.key == "Enter"&& newTask!="") {
+							console.log("from here"+newTask);
+							updateTodoList();
+						}
+					}} />
+					<div className="row d-flex">
+						<ul className="list-group d-flex justify-content-between">
+							{
+								list.map((todo, index) => {
+									return <li className="list-group-item d-flex" key={index}>{todo.label}
+										<button type="button" className="ms-auto btn btn-danger" onClick={() => {
+											deleteTodo(todo)
+										}}>DELETE</button>
+									</li>
+								})
+							}
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
